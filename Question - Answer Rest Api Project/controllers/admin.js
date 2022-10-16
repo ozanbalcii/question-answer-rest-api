a3604312e82582b1517e247b4e952ea6f6326d45
ozan <ozanbalcieem@gmail.com>
2022-10-16 21:54:49 +0300

    Create admin.js

---
+++ Question - Answer Rest Api Project/controllers/admin.js
@@ -0,0 +1,38 @@
+const CustomError = require('../helpers/error/CustomError');
+const asyncErrorWrapper = require("express-async-handler");  
+const User = require('../models/User');
+
+const blockUser = asyncErrorWrapper(async (req, res, next) => {
+
+    const { id } = req.params; // id a
+
+    const user =  await User.findById(id);
+
+    user.blocked = !user.blocked; 
+    await user.save(); 
+
+    return res.status(200)
+    .json({
+        success: true,
+        message: "Block - Unblock Successfull!"
+    });
+});
+
+const deleteUser = asyncErrorWrapper(async (req, res, next) => {
+
+    const { id } = req.params; // id 
+
+    const user = await User.findById(id);
+
+    await user.remove(); 
+    return res.status(200)
+    .json({
+        success: true,
+        message: "Delete Operation Successfull"
+    });
+});
+
+module.exports = {
+    blockUser,
+    deleteUser
+}
