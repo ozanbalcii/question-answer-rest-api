7a0df830ef3d5d08f6fb0cd0dec70552bd708ee7
ozan <ozanbalcieem@gmail.com>
2022-10-26 15:36:03 +0300

    Create Answer.js

---
+++ Question - Answer Rest Api Project/models/Answer.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const Question = require('./Question');
+const AnswerSchema = new Schema({
+
+    content : { 
+        type : String,
+        required : [true, "Please provide a Content "],
+        minlength : [10,"please provide a title at least 10 characters"]
+    },
+    createdAt : {
+        type : Date,
+        default : Date.now
+    },
+    Likes : [ 
+        {
+            type : mongoose.Schema.ObjectId,
+            ref : "User"
+        }
+    ],
+    user : { 
+        type : mongoose.Schema.ObjectId,
+        ref : "User",
+        required : true
+    },
+    question : { 
+        type : mongoose.Schema.ObjectId,
+        ref: "Question",
+        required : true
+    }
+});
+
+AnswerSchema.pre("save", async function(next){
+    if(!this.isModified("user")) return next();
+
+try{
+    const question = await Question.findById(this.question);   
+    question.answers.push(this._id); 
+    question.answerCount =  question.answers.length;  
+    await question.save(); 
+    next();
+    }
+
+ catch(err){
+        return next(err); 
+    };
+});
+
+module.exports = mongoose.model("Answer",AnswerSchema);
