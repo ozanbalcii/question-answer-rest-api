31f64baa0a194bdb5aa933b916d7b51ae2667e78
ozan <ozanbalcieem@gmail.com>
2022-10-31 09:34:07 +0300

    Create server.js

---
+++ Question - Answer Rest Api Project/server.js
@@ -0,0 +1,27 @@
+
+const express  = require('express'); 
+const dotenv = require('dotenv');
+const connectDatabase= require('./helpers/database/connectDatabase'); 
+const routers = require('./routers');
+const customErrorHandler = require('./middleware/errors/customErrorHandler');
+const path = require('path'); 
+
+ dotenv.config({ 
+    path : "./config/env/config.env"
+ });
+
+connectDatabase();
+const app = express();
+app.use(express.json());  
+
+const PORT = process.env.PORT; 
+
+app.use(customErrorHandler); 
+
+console.log(__dirname); 
+
+app.use(express.static(path.join(__dirname, 'public'))); 
+app.listen(PORT,() =>{
+
+    console.log('App Started on ${PORT} : ${process.env.NODE_ENV}'); 
+});
